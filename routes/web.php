24e922402c9bc5820012e452fe5b636f076a5ff6
Syafiq Naim <syafiqnaim@Syafiqs-MacBook-Pro.local>
2023-02-13 14:54:38 +0800

    13022023

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo "Hello! Welcome to 2023-2, today's time is " . date("h:i:sa") . "(UTC) and the date is " . date("d-m-Y") . ". Also not to forgot, it is a " . date("l") . ".";
+    echo "Hello! Welcome to 2023-3, today's time is " . date("h:i:sa") . "(UTC) and the date is " . date("d-m-Y") . ". Also not to forgot, it is a " . date("l") . ".";
 });
